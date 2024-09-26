086e069a4a34cee71ccba6d15a377357a3251af1
Aisly <aisly.agdavletova@gmail.com>
2024-09-26 22:34:40 +0300

    Initial commit

---
+++ m2m-relations/articles/admin.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+
+from .models import Article, Scope, Tag
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
+
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        for form in self.forms:
+            # В form.cleaned_data будет словарь с данными
+            # каждой отдельной формы, которые вы можете проверить
+            form.cleaned_data
+            # вызовом исключения ValidationError можно указать админке о наличие ошибки
+            # таким образом объект не будет сохранен,
+            # а пользователю выведется соответствующее сообщение об ошибке
+            raise ValidationError('Тут всегда ошибка')
+        return super().clean()  # вызываем базовый код переопределяемого метода
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    formset = ScopeInlineFormset
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [ScopeInline]
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
+
